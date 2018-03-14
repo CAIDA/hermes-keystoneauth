1aacf054f77a6e213a039d9a3516ad7d797f4b4e
Alistair King <alistair@caida.org>
2018-03-14 12:27:38 -0700

    Add copyright notice

--- hermes_keystoneauth/middleware.py
+++ hermes_keystoneauth/middleware.py
@@ -1,5 +1,9 @@
 # Copyright 2012 OpenStack Foundation
 #
+# This code is forked from the standard Swift keystoneauth middleware
+# Modifications are
+#   Copyright 2018 The Regents of the University of California.
+#
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
 # a copy of the License at
